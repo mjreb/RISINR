ce5e4daed8499a9e35d9dab0fe328a605656b56f
mjreb <mjrbcontacto@gmail.com>
2025-06-05 14:31:53 -0600

    Interfaz de jpa para hacer los querys

--- RISjwt/src/main/java/com/RIS/MVC/model/CRUDrepository/EquipoImagenologiaRepository.java
+++ RISjwt/src/main/java/com/RIS/MVC/model/CRUDrepository/EquipoImagenologiaRepository.java
@@ -5,11 +5,18 @@
 package com.RIS.MVC.model.CRUDrepository;
 
 import com.RIS.MVC.model.JPA.entities.EquipoImagenologia;
+import java.util.Date;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface EquipoImagenologiaRepository extends JpaRepository<EquipoImagenologia, String> {
-   
     
+    EquipoImagenologia findByNombre(String Nombre);
+    List<EquipoImagenologia> findByMarca(String marca);
+    List<EquipoImagenologia> findByModelo(String modelo);
+    List<EquipoImagenologia> findByModalidad(String modalidad);
+    List<EquipoImagenologia> findByFechaInstalacion(Date fechaInstalacion); 
+    List<EquipoImagenologia> findByEstado(String estado);
 }
